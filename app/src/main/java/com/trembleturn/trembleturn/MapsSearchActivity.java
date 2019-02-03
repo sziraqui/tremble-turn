ec10e3d82d1f8fdf88e26d4a35ab8446806420b7
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-02-03 16:26:24 +0530

    Grant GPS permission successfully

--- app/src/main/java/com/trembleturn/trembleturn/MapsSearchActivity.java
+++ app/src/main/java/com/trembleturn/trembleturn/MapsSearchActivity.java
@@ -1,21 +1,61 @@
 package com.trembleturn.trembleturn;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-public class MapsSearchActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsSearchActivity extends BaseActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, ResultCallback<LocationSettingsResult> {
+
+    public static final int REQUEST_LOCATION_CODE = 99;
+    public static final int RC_RESOLVE_GPS_PERMISSION = 10001;
 
     private static final String TAG = MapsSearchActivity.class.getSimpleName();
-
     private Toolbar toolbar;
     private EditText etDest;
     private ImageView ivSearch;
     private FloatingActionButton fab;
+
+    private GoogleMap mMap;
+    private GoogleApiClient client;
+    private LocationRequest locationRequest;
+    private Location lastLocation;
+    private Marker currentLocationMarker;
+
 
     public void init() {
         toolbar = findViewById(R.id.toolbar);
@@ -25,14 +65,128 @@ public class MapsSearchActivity extends AppCompatActivity {
         fab = findViewById(R.id.fab);
     }
 
+    public SupportMapFragment initializeMap() {
+        SupportMapFragment mapFragment = new SupportMapFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.maps_container, mapFragment)
+                .commit();
+        mapFragment.getMapAsync(this);
+        return mapFragment;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+
+    protected synchronized void buildGoogleApiClient() {
+        client = new GoogleApiClient.Builder(this).
+                addConnectionCallbacks(this).
+                addOnConnectionFailedListener(this).
+                addApi(LocationServices.API).
+                build();
+        client.connect();
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi
+                .checkLocationSettings(client, builder.build());
+        result.setResultCallback(this);
+    }
+
+    // result of location settings request
+    @Override
+    public void onResult(@NonNull LocationSettingsResult result) {
+        final Status status = result.getStatus();
+        final LocationSettingsStates state = result
+                .getLocationSettingsStates();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                Log.d(TAG, "GPS permission granted successfully");
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                try {
+                    Log.d(TAG, "Attempting GPS permission resolution");
+                    status.startResolutionForResult(this, RC_RESOLVE_GPS_PERMISSION);
+                } catch (IntentSender.SendIntentException e) {
+                    // Ignore the error.
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                Log.d(TAG, "Impossible to get GPS permission");
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_RESOLVE_GPS_PERMISSION) {
+            if(resultCode == Activity.RESULT_OK){
+                Log.d(TAG, "GPS permission granted through resolution");
+            } if (resultCode == Activity.RESULT_CANCELED) {
+                Log.d(TAG, "GPS permission resolution failed");
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lastLocation = location;
+
+        if(currentLocationMarker!=null)
+        {
+            currentLocationMarker.remove();
+        }
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Location");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+        currentLocationMarker = mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
+
+        if(client!=null)
+        {
+            LocationServices.FusedLocationApi.removeLocationUpdates(client, this);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_search);
         init();
+        buildGoogleApiClient();
     }
-
 
 
     public void onClick(View v) {
@@ -45,7 +199,7 @@ public class MapsSearchActivity extends AppCompatActivity {
                 // get directions
                 // show start FAB
                 break;
-
         }
     }
+
 }
