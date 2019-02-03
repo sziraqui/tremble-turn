65858351436b766628755ce849162316431f9578
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-02-03 18:23:17 +0530

    show simulation

--- app/src/main/java/com/trembleturn/trembleturn/MapsSearchActivity.java
+++ app/src/main/java/com/trembleturn/trembleturn/MapsSearchActivity.java
@@ -68,20 +68,56 @@ public class MapsSearchActivity extends BaseActivity implements
 
     public static final int REQUEST_LOCATION_CODE = 99;
     public static final int RC_RESOLVE_GPS_PERMISSION = 10001;
+    public static final int SIMULATION_DELAY = 3000;
+    public static final int VIBRATION_DELAY = 1000;
 
     private static final String TAG = MapsSearchActivity.class.getSimpleName();
+    protected Routes routes;
     private Toolbar toolbar;
     private EditText etDest;
     private ImageView ivSearch;
     private FloatingActionButton fab;
-
     private GoogleMap mMap;
     private GoogleApiClient client;
     private LocationRequest locationRequest;
     private Location lastLocation;
     private Marker currentLocationMarker;
-    protected Routes routes;
 
+    public static List<LatLng> decode(final String encodedPath) {
+        int len = encodedPath.length();
+
+        // For speed we preallocate to an upper bound on the final length, then
+        // truncate the array before returning.
+        final List<LatLng> path = new ArrayList<LatLng>();
+        int index = 0;
+        int lat = 0;
+        int lng = 0;
+
+        while (index < len) {
+            int result = 1;
+            int shift = 0;
+            int b;
+            do {
+                b = encodedPath.charAt(index++) - 63 - 1;
+                result += b << shift;
+                shift += 5;
+            } while (b >= 0x1f);
+            lat += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+
+            result = 1;
+            shift = 0;
+            do {
+                b = encodedPath.charAt(index++) - 63 - 1;
+                result += b << shift;
+                shift += 5;
+            } while (b >= 0x1f);
+            lng += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+
+            path.add(new LatLng(lat * 1e-5, lng * 1e-5));
+        }
+
+        return path;
+    }
 
     public void init() {
         toolbar = findViewById(R.id.toolbar);
@@ -217,11 +253,10 @@ public class MapsSearchActivity extends BaseActivity implements
         List<Address> addressList = null;
         MarkerOptions mo = new MarkerOptions();
         LatLng latlng = new LatLng(19.022231, 72.856226);
-        if(!location.equals(""))
-        {
+        if (!location.equals("")) {
             Geocoder geocoder = new Geocoder(this);
             try {
-                addressList=geocoder.getFromLocationName(location, 5);
+                addressList = geocoder.getFromLocationName(location, 5);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -241,13 +276,45 @@ public class MapsSearchActivity extends BaseActivity implements
 
             case R.id.iv_search:
                 String dest = etDest.getText().toString();
+                LatLng source;
                 LatLng destLatLng = showDestinationOnMap(dest);
-                LatLng source = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                try {
+                    source = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                } catch (Exception e) {
+                    source = new LatLng(19.022231, 72.856226);
+                }
+
                 getAtoBSteps(source, destLatLng);
                 // show start FAB
                 break;
             case R.id.fab:
                 // Start vibrations
+                int sizeOfRoute = routes.legs.get(0).steps.size();
+
+                int i;
+                List<LatLng> latlnglist = new ArrayList<LatLng>();
+                List<String> manevuer = new ArrayList<String>();
+                List<String> instr = new ArrayList<String>();
+
+                for (i = 0; i < sizeOfRoute; i++) {
+                    double lat1 = routes.legs.get(0).steps.get(i).end_location.lat;
+                    double lng1 = routes.legs.get(0).steps.get(i).end_location.lng;
+                    latlnglist.add(new LatLng(lat1, lng1));
+
+                    String m2 = routes.legs.get(0).steps.get(i).htmlinstructions;
+                    instr.add(m2);
+
+                    String m1 = routes.legs.get(0).steps.get(i).maneuver;
+                    if (m1 == null)
+                        manevuer.add("Keep going");
+                    else
+                        manevuer.add(m1);
+
+                }
+
+                Log.i(TAG, latlnglist.toString());
+
+                setAnimation(mMap, latlnglist, manevuer, instr);
 
         }
     }
@@ -259,7 +326,7 @@ public class MapsSearchActivity extends BaseActivity implements
             public void run() {
                 stopVibrateLeft();
             }
-        }, 1000);
+        }, VIBRATION_DELAY);
 
     }
 
@@ -270,7 +337,7 @@ public class MapsSearchActivity extends BaseActivity implements
             public void run() {
                 stopVibrateRight();
             }
-        }, 1000);
+        }, VIBRATION_DELAY);
     }
 
     private void vibrateLeft() {
@@ -325,7 +392,7 @@ public class MapsSearchActivity extends BaseActivity implements
                 try {
                     routes = new Gson().fromJson(response.getJSONArray("routes").get(0).toString(), Routes.class);
                     Log.i(TAG, routes.legs.get(0).start_location.lat + " " + routes.legs.get(0).steps.get(0).end_location.lat);
-                    String []pathdisplay = getPaths(routes);
+                    String[] pathdisplay = getPaths(routes);
                     directiondisplay(pathdisplay);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -341,11 +408,11 @@ public class MapsSearchActivity extends BaseActivity implements
         }
     }
 
-    public String[] getPaths(Routes route){
+    public String[] getPaths(Routes route) {
 
         List<Steps> step = route.legs.get(0).steps;
         String routes[] = new String[step.size()];
-        for(int i=0;i<step.size();i++){
+        for (int i = 0; i < step.size(); i++) {
 
             routes[i] = getPath(step.get(i));
         }
@@ -353,18 +420,15 @@ public class MapsSearchActivity extends BaseActivity implements
         return routes;
     }
 
-    public String getPath(Steps step){
+    public String getPath(Steps step) {
 
         String polyline = step.polyline.points;
         return polyline;
     }
 
-    public void directiondisplay(String[] path){
+    public void directiondisplay(String[] path) {
 
-
-        for(int i =0 ; i<path.length ; i++){
-
-
+        for (int i = 0; i < path.length; i++) {
 
             PolylineOptions options = new PolylineOptions();
             options.color(Color.RED);
@@ -375,76 +439,50 @@ public class MapsSearchActivity extends BaseActivity implements
         }
     }
 
-    public static List<LatLng> decode(final String encodedPath) {
-        int len = encodedPath.length();
-
-        // For speed we preallocate to an upper bound on the final length, then
-        // truncate the array before returning.
-        final List<LatLng> path = new ArrayList<LatLng>();
-        int index = 0;
-        int lat = 0;
-        int lng = 0;
-
-        while (index < len) {
-            int result = 1;
-            int shift = 0;
-            int b;
-            do {
-                b = encodedPath.charAt(index++) - 63 - 1;
-                result += b << shift;
-                shift += 5;
-            } while (b >= 0x1f);
-            lat += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
-
-            result = 1;
-            shift = 0;
-            do {
-                b = encodedPath.charAt(index++) - 63 - 1;
-                result += b << shift;
-                shift += 5;
-            } while (b >= 0x1f);
-            lng += (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
-
-            path.add(new LatLng(lat * 1e-5, lng * 1e-5));
-        }
-
-        return path;
-    }
-
-
     @Override
     public void onError(int requestCode, ErrorType errorType, JSONObject response) {
 
     }
 
-    public void setAnimation(GoogleMap myMap, final List<LatLng> directionPoint) {
+    public void setAnimation(GoogleMap myMap, final List<LatLng> directionPoint, final List<String> man, final List<String> instr) {
         Marker marker = myMap.addMarker(new MarkerOptions()
                 .position(directionPoint.get(0))
                 .flat(true));
 
         myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(directionPoint.get(0), 10));
 
-        animateMarker(myMap, marker, directionPoint, false);
+        animateMarker(myMap, marker, directionPoint, man, instr, false);
     }
 
-    private void animateMarker(final GoogleMap myMap, final Marker marker, final List<LatLng> directionPoint, final boolean hideMarker) {
+    private void animateMarker(final GoogleMap myMap, final Marker marker, final List<LatLng> directionPoint, final List<String> man, final List<String> instr, final boolean hideMarker) {
         final Handler handler = new Handler();
         final long start = SystemClock.uptimeMillis();
         Projection proj = myMap.getProjection();
-        final long duration = 30000;
+        final long duration = Integer.MAX_VALUE;
 
         final Interpolator interpolator = new LinearInterpolator();
 
         handler.post(new Runnable() {
             int i = 0;
 
+
             @Override
             public void run() {
                 long elapsed = SystemClock.uptimeMillis() - start;
                 float t = interpolator.getInterpolation((float) elapsed / duration);
                 if (i < directionPoint.size()) {
+
                     marker.setPosition(directionPoint.get(i));
                     myMap.animateCamera(CameraUpdateFactory.newLatLng(directionPoint.get(i)));
+
+                    if (man.get(i).contains("right")) {
+                        indicateRight();
+                    } else if (man.get(i).contains("left")) {
+                        indicateLeft();
+                    } else {
+
+                    }
+                    Log.d("maneuver", man.get(i));
 
                 }
                 //myMap.animateCamera(CameraUpdateFactory.zoomTo(2.0f));
@@ -453,7 +491,7 @@ public class MapsSearchActivity extends BaseActivity implements
 
                 if (t < 1.0) {
                     // Post again 16ms later.
-                    handler.postDelayed(this, 1000);
+                    handler.postDelayed(this, SIMULATION_DELAY);
                 } else {
                     if (hideMarker) {
                         marker.setVisible(false);
