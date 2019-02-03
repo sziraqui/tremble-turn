0dabe4df683b1720512a81e801b373b4e9c8f89d
Sarfaraz Iraqui <sarfarazghlm@gmail.com>
2019-02-03 15:30:54 +0530

    initialize main ui

--- app/src/main/java/com/trembleturn/trembleturn/MapsSearchActivity.java
+++ app/src/main/java/com/trembleturn/trembleturn/MapsSearchActivity.java
@@ -3,6 +3,7 @@ package com.trembleturn.trembleturn;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -11,16 +12,28 @@ public class MapsSearchActivity extends AppCompatActivity {
 
     private static final String TAG = MapsSearchActivity.class.getSimpleName();
 
+    private Toolbar toolbar;
     private EditText etDest;
     private ImageView ivSearch;
     private FloatingActionButton fab;
+
+    public void init() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        etDest = findViewById(R.id.et_dest);
+        ivSearch = findViewById(R.id.iv_search);
+        fab = findViewById(R.id.fab);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_search);
-
+        init();
     }
+
+
 
     public void onClick(View v) {
         switch (v.getId()) {
